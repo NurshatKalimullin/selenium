831d4bd4432100697a696282cd9457137bf316ca
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2021-12-12 19:29:02 +0300

    Set path to FireFox browser

--- litecarttests/src/test/java/ru/stqa/selenium/litecart/appmanager/ApplicationManager.java
+++ litecarttests/src/test/java/ru/stqa/selenium/litecart/appmanager/ApplicationManager.java
@@ -1,10 +1,16 @@
 package ru.stqa.selenium.litecart.appmanager;
 
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 import java.io.FileReader;
@@ -34,12 +40,30 @@ public class ApplicationManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
+            //let's use FireFox Nightly
+            FirefoxBinary firefoxBinary = new FirefoxBinary(
+                    new File("C:\\Program Files\\Firefox Nightly\\firefox.exe"));
+            DesiredCapabilities desired = DesiredCapabilities.firefox();
+            FirefoxOptions options = new FirefoxOptions();
+            desired.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options.setBinary(firefoxBinary));
+
+            wd = new FirefoxDriver(options);
         } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
+            //open maximized Chrome explorer
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-maximized");
+
+            //set browser setting and add it to browser
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability(ChromeOptions.CAPABILITY, options);
+            wd = new ChromeDriver(caps);
         } else if (browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
         }
+
+        //let's print browser settings on console
+        System.out.println(((HasCapabilities) wd).getCapabilities());
+
 
         //if element is not presented test will wait 1 seconds for element to load
         wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
