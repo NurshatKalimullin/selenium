2a3af04b69a4075d3be0c3796df3490b3961a314
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2021-12-26 17:36:04 +0300

    Task#10 and separate initiation for shop and admin

---
+++ litecarttests/src/test/java/ru/stqa/selenium/litecart/tests/ShopTestBase.java
@@ -0,0 +1,39 @@
+package ru.stqa.selenium.litecart.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.selenium.litecart.appmanager.ApplicationManager;
+
+
+public class TestBase {
+
+
+    protected static final ApplicationManager app
+            = new ApplicationManager (System.getProperty("browser", BrowserType.EDGE));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp(ITestContext context) throws Exception {
+        app.initShop();
+        context.setAttribute("app", app);
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
