fe323517a653ee483401c4a0ec95bb0d2d1f84be
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2022-01-04 21:48:56 +0300

    Task #10 Change color tests of merchandise

--- litecarttests/src/test/java/ru/stqa/selenium/litecart/tests/LitecartShopTests.java
+++ litecarttests/src/test/java/ru/stqa/selenium/litecart/tests/LitecartShopTests.java
@@ -8,7 +8,9 @@ import org.testng.annotations.Test;
 import ru.stqa.selenium.litecart.model.CustomerData;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import static org.testng.Assert.assertEquals;
 
@@ -27,24 +29,32 @@ public class LitecartShopTests extends ShopTestBase {
     @Test
     public void testMerchandisePage() {
         String expectedLineThroughStyleValue = "line-through";
-        String expectedMerchandiseCampaignPriceColor = "#cc0000";
+        String expectedMerchandiseCampaignPriceColor = "0";
 
         List<WebElement> goods = app.getNavigationHelper().getElementsList(By.xpath("//div[@id='box-campaigns']//li[contains(@class,'product')]"));
         String merchandiseName = app.getSessionHelper().getAttributeValue(goods.get(0).findElement(By.xpath(".//div[@class='name']")), "textContent");
         String merchandiseRegularPrice = app.getSessionHelper().getAttributeValue(goods.get(0).findElement(By.xpath(".//s[@class='regular-price']")), "textContent");
         String merchandiseCampaignPrice = app.getSessionHelper().getAttributeValue(goods.get(0).findElement(By.xpath(".//strong[@class='campaign-price']")), "textContent");
-        String merchandiseRegularPriceColor = Color.fromString(goods.get(0).findElement(By.xpath(".//s[@class='regular-price']")).getCssValue("color")).asHex();
+
+        String[] merchandiseRegularPriceColor = goods.get(0).findElement(By.xpath(".//s[@class='regular-price']")).getCssValue("color")
+                .replace("rgba(", "").replace(")", "").replace(" ", "").split(",");
+
         String merchandiseRegularPriceLine = goods.get(0).findElement(By.xpath(".//s[@class='regular-price']")).getCssValue("text-decoration-line");
-        String merchandiseCampaignPriceColor = Color.fromString(goods.get(0).findElement(By.xpath(".//strong[@class='campaign-price']")).getCssValue("color")).asHex();
+
+        String[] merchandiseCampaignPriceColor = goods.get(0).findElement(By.xpath(".//strong[@class='campaign-price']")).getCssValue("color")
+                .replace("rgba(", "").replace(")", "").replace(" ", "").split(",");
+
         String merchandiseCampaignPriceStyle = goods.get(0).findElement(By.xpath(".//strong[@class='campaign-price']")).getCssValue("font-weight");
         String merchandiseRegularPriceSizeString = goods.get(0).findElement(By.xpath(".//s[@class='regular-price']")).getCssValue("font-size");
         String merchandiseCampaignPriceSizeString = goods.get(0).findElement(By.xpath(".//strong[@class='campaign-price']")).getCssValue("font-size");
         Float merchandiseRegularPriceSize = Float.parseFloat(merchandiseRegularPriceSizeString.substring(0, merchandiseRegularPriceSizeString.length() - 2));
         Float merchandiseCampaignPriceSize = Float.parseFloat(merchandiseCampaignPriceSizeString.substring(0, merchandiseCampaignPriceSizeString.length() - 2));
 
-        Assert.assertEquals(merchandiseRegularPriceColor, "#777777");
+        Assert.assertTrue(merchandiseRegularPriceColor[0].equals(merchandiseRegularPriceColor[1])
+                && merchandiseRegularPriceColor[0].equals(merchandiseRegularPriceColor[2]));
         Assert.assertEquals(merchandiseRegularPriceLine, expectedLineThroughStyleValue);
-        Assert.assertEquals(merchandiseCampaignPriceColor, expectedMerchandiseCampaignPriceColor);
+        Assert.assertTrue(merchandiseCampaignPriceColor[1].equals(expectedMerchandiseCampaignPriceColor)
+                && merchandiseCampaignPriceColor[2].equals(expectedMerchandiseCampaignPriceColor));
         Assert.assertTrue(merchandiseCampaignPriceStyle.equals("700") || merchandiseCampaignPriceStyle.equals("900"));
         Assert.assertTrue(merchandiseRegularPriceSize < merchandiseCampaignPriceSize);
 
@@ -55,14 +65,18 @@ public class LitecartShopTests extends ShopTestBase {
         Assert.assertEquals(app.getSessionHelper().getAttributeValue(merchandise.findElement(By.xpath(".//s[@class='regular-price']")), "textContent"), merchandiseRegularPrice);
         Assert.assertEquals(app.getSessionHelper().getAttributeValue(merchandise.findElement(By.xpath(".//strong[@class='campaign-price']")), "textContent"), merchandiseCampaignPrice);
 
-        merchandiseRegularPriceColor = Color.fromString(merchandise.findElement(By.xpath(".//s[@class='regular-price']")).getCssValue("color")).asHex();
-        Assert.assertEquals(merchandiseRegularPriceColor, "#666666");
+        merchandiseRegularPriceColor = merchandise.findElement(By.xpath(".//s[@class='regular-price']")).getCssValue("color")
+                .replace("rgba(", "").replace(")", "").replace(" ", "").split(",");
+        Assert.assertTrue(merchandiseRegularPriceColor[0].equals(merchandiseRegularPriceColor[1])
+                && merchandiseRegularPriceColor[0].equals(merchandiseRegularPriceColor[2]));
 
         merchandiseRegularPriceLine = merchandise.findElement(By.xpath(".//s[@class='regular-price']")).getCssValue("text-decoration-line");
         Assert.assertEquals(merchandiseRegularPriceLine, expectedLineThroughStyleValue);
 
-        merchandiseCampaignPriceColor = Color.fromString(merchandise.findElement(By.xpath(".//strong[@class='campaign-price']")).getCssValue("color")).asHex();
-        Assert.assertEquals(merchandiseCampaignPriceColor, expectedMerchandiseCampaignPriceColor);
+        merchandiseCampaignPriceColor = merchandise.findElement(By.xpath(".//strong[@class='campaign-price']")).getCssValue("color")
+                .replace("rgba(", "").replace(")", "").replace(" ", "").split(",");;
+        Assert.assertTrue(merchandiseCampaignPriceColor[1].equals(expectedMerchandiseCampaignPriceColor)
+                && merchandiseCampaignPriceColor[2].equals(expectedMerchandiseCampaignPriceColor));
 
         merchandiseCampaignPriceStyle = merchandise.findElement(By.xpath(".//strong[@class='campaign-price']")).getCssValue("font-weight");
         Assert.assertTrue(merchandiseCampaignPriceStyle.equals("700") || merchandiseCampaignPriceStyle.equals("900"));
